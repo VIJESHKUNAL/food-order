dda8e0ff43a5dfb6e507344b027fe8392be34d13
Vijesh Kunal <lohanvijeshkunalee@gmail.com>
2024-05-19 23:23:02 +0530

    start

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-content">
+        <h1 className="about-title">About Us</h1>
+        <p className="about-text">
+          We are passionate about delivering the best food experience...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/home.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="hero-section">
+        <h1 className="hero-text">Welcome to Foodie's Paradise</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
